3df3f2ac954ae7a235ca525518016590bf84539e
Sampat Kumar <Sampathkumar871@gmail.com>
2018-07-30 15:11:44 +0530

    //New changes
    //New changes

---
+++ Factorial_Recursion/Factorial_Recursion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial_Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value to find the Factorial in recursion Method");
+            string number = Console.ReadLine();
+            int num = Convert.ToInt32(number);
+            int result = Factorial(num);
+            Console.WriteLine("Factrial value is:{0}", result);
+            Console.Read();
+        }
+
+        private static int Factorial(int num)
+        {
+            if (num == 0)
+            {
+                return 1;
+            }
+            return num * Factorial(num - 1);
+        }
+    }
+}
